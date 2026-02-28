2a8afb97d56330471df61efd2b96dc51467ddd08
matheusjose04 <otakinhosad245@gmail.com>
2026-02-28 17:03:04 -0300

    fazendo a parte de tarefa basico agora vou para igraja

---
+++ src/models/tarefa.py
@@ -0,0 +1,12 @@
+# criando a class tarefa
+class Tarefa:
+    def __init__(self, titulo, descricao, prioridade, ):
+        self._titulo = titulo
+        self._descricao = descricao
+        self._prioridade = max(1, min(prioridade, 5))
+        self._concluida = False
+
+    def marcar_concluida(self):
+        self._concluida = True
+    def marcar_pendente(self):
+        self._concluida = False
